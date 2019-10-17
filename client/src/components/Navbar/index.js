ed41ee96c2d4d34a2ae033972a35fb6e997cf1ff
Harrison <harrisonekpobimi@gmail.com>
2019-10-17 12:40:52 +0200

    feat: add navigation links

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nav from "./Nav"
+import LoginNav from "./LoginNav"
+
+const Index = () => {
+  // const user = sessionStorage.getItem(USER, "user")
+  return (
+    <div>
+      {/* {
+        user ? <LoginNav /> : <Nav />
+      } */}
+    </div>
+  );
+};
+
+export default Index;
